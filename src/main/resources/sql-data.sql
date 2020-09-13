1d612d99be65be8424db5378e608d9e5754cba90
Liam <LHarrold@qa.com>
2020-09-13 14:00:29 +0100

    populated customers/items database and set up test database configuration

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,2 +1,12 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `ims`.`items` (`item_name`,`price`,`description`) VALUES ('TV',249.99,"32 inch");
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('liam', 'harrold');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('john', 'smith');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('leo', 'messi');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('darth', 'vader');
+
+
+INSERT INTO `ims`.`items` (`item_name`,`price`,`description`) VALUES ('TV',249.99,'32 inch');
+INSERT INTO `ims`.`items` (`item_name`,`price`,`description`) VALUES ('Chair',99.99,'gaming chair');
+INSERT INTO `ims`.`items` (`item_name`,`price`,`description`) VALUES ('Book',14.99,'autobiography');
+INSERT INTO `ims`.`items` (`item_name`,`price`,`description`) VALUES ('Laptop',350.00,'MAC book');
+INSERT INTO `ims`.`items` (`item_name`,`price`,`description`) VALUES ('Camera',199.99,'4k 16x zoom');
