586820a5217051ec3b88d430e4087066a78bdd1c
Liam <LHarrold@qa.com>
2020-09-16 14:32:43 +0100

    created OrderDAOTest

--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -9,48 +9,49 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.DBUtils;
 
-public class ItemDAOTest {
-
-	private final ItemDAO DAO = new ItemDAO();
-
+public class OrderDAOTest {
+	
+	private final OrderDAO DAO = new OrderDAO();
+	
 	@Before
 	public void setup() {
 		DBUtils.connect("src/test/resources/db.properties");
 		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
 	}
-
+	
 	@Test
 	public void testCreate() {
-		final Item created = new Item(2L, "chair", 99.99,"Furniture");
+		final Order created = new Order(2L,1L,"Essex");
 		assertEquals(created, DAO.create(created));
 	}
-
+	
 	@Test
 	public void testReadAll() {
-		List<Item> expected = new ArrayList<>();
-		expected.add(new Item(1L, "TV",249.99,"Tech"));
+		List<Order> expected = new ArrayList<>();
+		expected.add(new Order(1L,1L,"Essex"));
 		assertEquals(expected, DAO.readAll());
 	}
-
+	
 	@Test
 	public void testReadLatest() {
-		assertEquals(new Item(1L, "TV",249.99,"Tech"), DAO.readLatest());
+		assertEquals(new Order(1L,1L,"Essex"), DAO.readLatest());
 	}
-
+	
 	@Test
 	public void testRead() {
-		final long ID = 1L;
-		assertEquals(new Item(ID, "TV",249.99,"Tech"), DAO.readItem(ID));
+		final Long ID = 1L;
+		assertEquals(new Order(ID,1L,"Essex"), DAO.readOrder(ID));
 	}
-
+	
 	@Test
 	public void testUpdate() {
-		final Item updated = new Item(1L, "chair",49.99,"Furtniture");
+		final Order updated = new Order(1L,1L,"London");
 		assertEquals(updated, DAO.update(updated));
 	}
-
+	
 	@Test
 	public void testDelete() {
 		assertEquals(1, DAO.delete(1));
